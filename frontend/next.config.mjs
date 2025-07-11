909cae4427888f795499d681c8f802b7895066a3
Arpit Singhal <arpitsinghal4002@gmail.com>
2025-07-11 15:44:02 +0530

    did cler authorization

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      type: 'asset/resource',
-    });
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/avinashdm/gs-images/**',
+      },
+    ],
   },
 };
 
